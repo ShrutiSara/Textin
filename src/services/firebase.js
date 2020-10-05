46311977983766c63c7abd6e02520393295b2461
Shruti <52789421+shrued@users.noreply.github.com>
2020-10-05 14:52:21 +0530

    firebase hidden

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-    apiKey: "XXXXXXXXXXXX",
-    authDomain: "textin-textin.firebaseapp.com",
-    databaseURL: "https://textin-textin.firebaseio.com",
-    projectId: "textin-textin",
-    storageBucket: "textin-textin.appspot.com",
-    messagingSenderId: "624544357119",
-    appId: "1:624544357119:web:e3d7024b4ce0818cfe0aca",
-    measurementId: "G-WMNT661ER1"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(config);
